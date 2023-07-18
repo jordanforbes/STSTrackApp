02287794b3a234a3966b443d35118e8e6d4788c5
Jordan Forbes <jordforb@gmail.com>
2023-07-18 15:01:10 -0400

    iterates through folders to get runs rather than just has all files in one folder

--- lib/all_runs_importer.rb
+++ lib/all_runs_importer.rb
@@ -12,45 +12,53 @@ require_relative '../config/environment'
 folder_path = Rails.root.join('db/runs')
 
 puts "all runs importer"
+Dir.foreach(folder_path) do |entry|
+  puts entry
+  next unless File.directory?(File.join(folder_path, entry))
 
-json_files = Dir.glob(File.join(folder_path, '*.json'))
+  folder = File.join(folder_path, entry)
+  puts folder
 
-json_files.each do |file_path|
-  json_data = File.read(file_path)
-  record = JSON.parse(json_data)
+  json_files = Dir.glob(File.join(folder, '*.json'))
+  puts json_files
 
-  max_hp_per_floor = record["max_hp_per_floor"] || []  # Ensure max_hp_per_floor is an array
-  max_hp = max_hp_per_floor.last || 0  # Get the last element or use 0 if the array is empty
+  json_files.each do |file_path|
+    json_data = File.read(file_path)
+    record = JSON.parse(json_data)
 
-  # checks the final fight which the character had before the run ended
-  final_fight = record["damage_taken"].last || {"damage"=>0, "enemies"=>"None", "floor"=>0, "turns"=>0}
-  # if the last fight was the heart, and the character won the run, then obviously they killed the heart
-  heart_kill = false
+    max_hp_per_floor = record["max_hp_per_floor"] || []  # Ensure max_hp_per_floor is an array
+    max_hp = max_hp_per_floor.last || 0  # Get the last element or use 0 if the array is empty
 
-  # puts final_fight
-  if final_fight["enemies"] == "The Heart" && record["victory"] == true
-    heart_kill = true
+    # checks the final fight which the character had before the run ended
+    final_fight = record["damage_taken"].last || {"damage"=>0, "enemies"=>"None", "floor"=>0, "turns"=>0}
+    # if the last fight was the heart, and the character won the run, then obviously they killed the heart
+    heart_kill = false
+
+    # puts final_fight
+    if final_fight["enemies"] == "The Heart" && record["victory"] == true
+      heart_kill = true
+    end
+
+    # puts heart_kill
+
+    mapped_records = {
+      character: record["character_chosen"] == 'THE_SILENT'? 'SILENT' : record["character_chosen"],
+      floor: record["floor_reached"],
+      victory: record["victory"],
+      killed_by: record["victory"] ? nil : record["killed_by"],
+      ascension: record["ascension_level"],
+      gold: record["gold"],
+      max_hp: max_hp,
+      damage_taken: record["damage_taken"],
+      master_deck: "[" + record["master_deck"].join(", ") + "]",
+      relics: "[" + record["relics"].join(", ") + "]",
+      seed: record["seed_played"],
+      heart_kill: heart_kill,
+      local_time: BigDecimal(record["local_time"]),
+      play_id: record["play_id"],
+      run_id: record["run_id"]
+    }
+
+    StsRun.create(mapped_records)
   end
-
-  # puts heart_kill
-
-  mapped_records = {
-    character: record["character_chosen"] == 'THE_SILENT'? 'SILENT' : record["character_chosen"],
-    floor: record["floor_reached"],
-    victory: record["victory"],
-    killed_by: record["victory"] ? nil : record["killed_by"],
-    ascension: record["ascension_level"],
-    gold: record["gold"],
-    max_hp: max_hp,
-    damage_taken: record["damage_taken"],
-    master_deck: "[" + record["master_deck"].join(", ") + "]",
-    relics: "[" + record["relics"].join(", ") + "]",
-    seed: record["seed_played"],
-    heart_kill: heart_kill,
-    local_time: BigDecimal(record["local_time"]),
-    play_id: record["play_id"],
-    run_id: record["run_id"]
-  }
-
-  StsRun.create(mapped_records)
 end
